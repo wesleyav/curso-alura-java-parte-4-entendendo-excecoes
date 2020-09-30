b91c49f0ac0c9cc7bf02035cbbbf5e8389292512
Wesley <wesley.contato@hotmail.com>
2020-09-30 07:58:58 -0300

    Variação do Catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -3,19 +3,22 @@ public class Fluxo {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception " + msg);
+			ex.printStackTrace();
+		}
+
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		try {
-			metodo2();
-		} catch (ArithmeticException ex) {
-			System.out.println("ArithmeticException");
-		}
-		// metodo2();
+		metodo2();
 		System.out.println("Fim do metodo1");
+
 	}
 
 	private static void metodo2() {
@@ -23,6 +26,8 @@ public class Fluxo {
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 			int a = i / 0;
+			// Conta c = null;
+			// c.deposita();
 		}
 		System.out.println("Fim do metodo2");
 	}
