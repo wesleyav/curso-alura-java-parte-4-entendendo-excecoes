34bbe37b9be436ad5287b6450715ba1530650d42
Wesley <wesley.contato@hotmail.com>
2020-10-06 06:04:34 -0300

    Hierarquia de exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -5,7 +5,7 @@ public class Fluxo {
 		System.out.println("Ini do main");
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException ex) {
+		} catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -24,7 +24,7 @@ public class Fluxo {
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
 		
-		throw new ArithmeticException("deu errado");
+		throw new MinhaExcecao("deu muito errado");
 				
 		//System.out.println("Fim do metodo2");
 	}
