144866a9e7352c9625038a64952f74f1ebf24b45
Wesley <wesley.contato@hotmail.com>
2020-10-06 08:20:31 -0300

    Try with resources

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -3,17 +3,34 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 
-		Conexao con = null;
-
-		try {
-			con = new Conexao();
-			con.leDados();
-		} catch (IllegalStateException ex) {
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+		} catch(IllegalStateException ex) {
 			System.out.println("Deu erro na conexão");
-		} finally {
-			con.fecha();
 		}
-
+		
+		
 	}
-
 }
+		
+		
+		
+		// ----------------------
+		
+//		Conexao con = null;
+//
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexão");
+//		} finally {
+//			System.out.println("Finally");
+//			if (con != null) {
+//				con.close();
+//			}
+//		}
+//
+//	}
+
+//}
